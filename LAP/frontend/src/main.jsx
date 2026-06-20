e6b368472c59b7c452e91e96392474356ba305ce
Vishal-0401 <vishalyadav00234@gmail.com>
2026-06-20 15:09:59 +0530

    refactor: replace ToastRoot component with ToastContainer directly in main.jsx

--- LAP/frontend/src/main.jsx
+++ LAP/frontend/src/main.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { AppProviders } from './app/providers.jsx';
-import { router } from './app/router.jsx';
-import ToastRoot from './toast/ToastRoot.jsx';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { AppProviders } from "./app/providers.jsx";
+import { router } from "./app/router.jsx";
+import "./styles/index.css";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProviders>
-      <ToastRoot />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />{" "}
       <RouterProvider router={router} />
     </AppProviders>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
