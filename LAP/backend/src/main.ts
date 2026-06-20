eb479e21c04c9d83c372c35b2e1b1519816e3e58
Vishal-0401 <vishalyadav00234@gmail.com>
2026-06-20 10:56:48 +0530

    enhance logging in main.ts

--- LAP/backend/src/main.ts
+++ LAP/backend/src/main.ts
@@ -1,30 +1,55 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { Logger } from "@nestjs/common";
+import { AppModule } from "./app.module";
+import { GlobalExceptionFilter } from "./common/filters/global-exception.filter";
+import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
+import { TimeoutInterceptor } from "./common/interceptors/timeout.interceptor";
 
 async function bootstrap() {
+  const logger = new Logger("UserService");
+
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
-  app.setGlobalPrefix(config.get<string>('API_PREFIX') ?? 'api/');
+  app.setGlobalPrefix(config.get<string>("API_PREFIX") ?? "api/");
   app.use(helmet());
   app.use(cookieParser());
-  app.enableCors({ origin: config.get<string>('FRONTEND_URL'), credentials: true });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+  app.enableCors({
+    origin: config.get<string>("FRONTEND_URL"),
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new GlobalExceptionFilter());
-  app.useGlobalInterceptors(new ResponseInterceptor(), new TimeoutInterceptor());
- 
-  const swaggerConfig = new DocumentBuilder().setTitle('LAP Documentation API').setVersion('1.0').addBearerAuth().build();
-  SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swaggerConfig));
-  
-  await app.listen(config.get<number>('PORT') ?? 3000);
-  console.log(`Application is running on: ${config.get<number>('PORT') ?? 3000}`);
+  app.useGlobalInterceptors(
+    new ResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("LAP Documentation API")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  SwaggerModule.setup(
+    "docs",
+    app,
+    SwaggerModule.createDocument(app, swaggerConfig),
+  );
+
+  await app.listen(config.get<number>("PORT") ?? 3000);
+   logger.verbose(
+    `Application is running on port: ${config.get<number>("PORT") ?? 3000}`,
+  );
 }
 
 bootstrap();
